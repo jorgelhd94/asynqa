03349b2245da7538a403374549c65a395a4fd726
Jorge Luis Hernández Dueñas <jorge.hernandez@tropipay.com>
2026-04-09 22:23:40 -0300

    Refactor environment dialog footer layout, improve error message parsing, and customize toast styling

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -6,7 +6,18 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <Outlet />
-      <Toaster theme="dark" position="bottom-right" />
+      <Toaster
+        theme="dark"
+        position="bottom-right"
+        options={{
+          fill: "#1e1b2e",
+          roundness: 16,
+          styles: {
+            title: "!text-[--color-black-50]",
+            description: "!text-[--color-black-300]",
+          },
+        }}
+      />
       <TanStackRouterDevtools position="bottom-left" />
     </>
   ),
