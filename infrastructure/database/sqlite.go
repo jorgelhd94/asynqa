90bb7544c49af2dee564dbc15abf644480d80a3a
Jorge Luis Hernández Dueñas <jorgelhd94@gmail.com>
2026-04-14 17:23:13 -0300

    Add optimistic updates to queue pause/unpause/delete mutations and relocate SQLite database to user config directory

--- infrastructure/database/sqlite.go
+++ infrastructure/database/sqlite.go
@@ -1,14 +1,34 @@
 package database
 
 import (
+	"fmt"
 	"log/slog"
+	"os"
+	"path/filepath"
 
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
+func dbPath() (string, error) {
+	configDir, err := os.UserConfigDir()
+	if err != nil {
+		return "", fmt.Errorf("cannot resolve config directory: %w", err)
+	}
+	dir := filepath.Join(configDir, "AsynQA")
+	if err := os.MkdirAll(dir, 0o755); err != nil {
+		return "", fmt.Errorf("cannot create data directory: %w", err)
+	}
+	return filepath.Join(dir, "asynqa.db"), nil
+}
+
 func InitSqlite() (*gorm.DB, error) {
-	dialector := sqlite.Open("./asynqa.db")
+	path, err := dbPath()
+	if err != nil {
+		return nil, err
+	}
+	slog.Info("database path", "path", path)
+	dialector := sqlite.Open(path)
 	gormLevel := SlogLevelToGorm(slog.LevelInfo, true)
 	logger := NewSlogAdapter(slog.Default()).LogMode(gormLevel)
 
